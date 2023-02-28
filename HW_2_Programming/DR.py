058fb68391802ef181296e0b0344043aa49aed1e
6ixEyes <faridkamizi@gmail.com>
2023-02-27 21:23:06 -0500

    Finished Q1

--- HW_2_Programming/DR.py
+++ HW_2_Programming/DR.py
@@ -190,7 +190,15 @@ def LE(X, out_dim, num_neighbor=5):
 
 
 def PCA(X, out_dim):
-    """
+    """_summary_
+    TODO:
+        1. build mu
+        2. build the covariance matrix Sigma: a D-by-D matrix (numpy array).
+        3. We have provided code of how to compute W from Sigma
+    Useful tool:
+        1. `np.mean`: find the mean vector
+        2. `np.matmul`: for matrix-matrix multiplication
+        3. the builtin "reshape" and "transpose()" function of a numpy array
     Input:
         X: a D-by-N matrix (numpy array) of the input data
         out_dim: the desired output dimension
@@ -199,23 +207,19 @@ def PCA(X, out_dim):
         W: the projection matrix of PCA. Please represent it as a D-by-out_dim matrix (numpy array).
             The m-th column should correspond to the m-th largest eigenvalue of the covariance matrix.
             Each column of W must have a unit L2 norm.
-    Todo:
-        1. build mu
-        2. build the covariance matrix Sigma: a D-by-D matrix (numpy array).
-        3. We have provided code of how to compute W from Sigma
-    Useful tool:
-        1. np.mean: find the mean vector
-        2. np.matmul: for matrix-matrix multiplication
-        3. the builtin "reshape" and "transpose()" function of a numpy array
-    """
+    """    
+
 
     X = np.copy(X)
+
     D = X.shape[0] # feature dimension
     N = X.shape[1] # number of data instances
 
+
     ### Your job 1 starts here ###
-    mu = ...
-    Sigma = ...
+    mu = np.mean(X, axis=1).reshape(-1, 1)
+    difference = X - mu
+    Sigma = np.matmul( (difference), (difference).transpose() ) / N
 
     ### Your job 1 ends here ###
 
